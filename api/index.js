819aeddd4362a3524febe18783e6f1695bc05497
Digdarshan-1 <digdarshanmohanty1010@gmail.com>
2025-05-03 12:55:18 +0530

    Refactor API structure: consolidate server setup into index.js and remove app.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,24 @@
+const express = require('express');
 const serverless = require('serverless-http');
-const app = require('../app');
+const path = require('path');
+const mongoose = require('mongoose');
+const connectDB = require('../db/dbconnection');
+const blogRoutes = require('../routes/blogRoutes');
+const blogController = require('../controllers/blogController');
 
-module.exports = serverless(app); // 👈 
+const app = express();
+
+// Mongo DB connection
+connectDB();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Routes
+app.get('/', blogController.getHome);
+app.use('/blogs', blogRoutes);
+
+module.exports = serverless(app);
